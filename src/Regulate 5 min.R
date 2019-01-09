82137e4dcfbc98a3c111dd9a68d01278f4fb8ff2
Ziwen Yu <ziwenyu83@gmail.com>
2019-01-09 14:02:33 -0500

    Update Regulate 5 min.R

--- src/Regulate 5 min.R
+++ src/Regulate 5 min.R
@@ -53,3 +53,58 @@ Regular_5min=function(Dt)
         return
     
 }
+
+
+
+Round_Time=function(Datetime,minuts)
+    #minuts:  time step (minutes)
+{
+    mins=round(
+        difftime(Datetime,
+                 as.Date(Datetime),
+                 units='mins') %>% 
+            as.numeric /minuts)*minuts
+    
+    
+    return(as.POSIXct((as.Date(Datetime)+lubridate::minutes(mins)), 
+                      format = "%Y-%m-%d %H:%M:%S")
+    )
+    
+}
+
+Regular_Time=function(Dt,minuts)
+    #minuts:  time step (minutes)
+    # First column is DateTime
+{
+    
+    let(list(TIME=colnames(Dt)[1]),
+        Dt %<>% 
+            mutate(TIME=Round_Time(TIME,minuts)) 
+    )
+    
+    let(list(TIME=colnames(Dt)[1]),
+        
+        Dt %>% 
+            select(TIME) %>% 
+            summarize(min(TIME),max(TIME)) -> Dt_rng
+    )
+    
+    
+    colnames(Dt_rng)=c('MinDt','MaxDt')
+    
+    Dt_rng %<>% 
+        mutate(MinDt=Round_Time(MinDt,minuts),
+               MaxDt=Round_Time(MaxDt,minuts))
+    
+    Tm_srs=seq(
+        from=Dt_rng$MinDt[1],
+        to=Dt_rng$MaxDt[1],
+        by=60*minuts
+    ) 
+    rm(Dt_rng)
+    
+    data.frame(Time=Tm_srs) %>%  
+        left_join(.,Dt,by=c('Time'=colnames(Dt)[1])) %>% 
+        return
+    
+}
